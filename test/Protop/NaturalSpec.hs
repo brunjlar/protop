71f65e1d5973c1e6f893fd748a61c1f3f3deefda
brunjlar <brunjlar@gmail.com>
2016-02-10 00:20:19 +0100

    added test suite

---
+++ test/Protop/NaturalSpec.hs
@@ -0,0 +1,21 @@
+module Protop.NaturalSpec (spec) where
+
+import Protop
+import Test.Hspec
+
+spec :: Spec
+spec = do
+    addSpec
+    mulSpec
+
+addSpec :: Spec
+addSpec = describe "add" $
+
+    it "should add natural numbers" $
+        (add .$. (42, 8)) `shouldBe` 50
+
+mulSpec :: Spec
+mulSpec = describe "mul" $
+
+    it "should multiply natural numbers" $
+        (mul .$. (11, 12)) `shouldBe` 132
